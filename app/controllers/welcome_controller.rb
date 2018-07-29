3cd035e93cffd8eeb867bd13cd158abf8383d1c0
Tyler Westlie <tyler.westlie@gmail.com>
2018-07-29 16:14:54 -0600

    removed commented code from routes, added filters

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-
     if params[:tag]
       @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
       @list = list_tags(@tutorials).uniq
--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,9 @@ Rails.application.routes.draw do
   get '/about', to: 'about#show'
   get '/get_started', to: 'get_started#show'
 
+  get '/video' ,to: 'video#show'
+
   resources :users, only: [:new, :create, :update, :edit]
   resources :tutorials, only: [:show, :index]
-  resources :videos, only: [:show, :index]
   resources :user_videos, only:[:create, :destroy]
 end
