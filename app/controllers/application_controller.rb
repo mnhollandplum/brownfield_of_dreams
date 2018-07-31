b6a614a7b1a506197b96487c420507be3ad31b76
Tyler Westlie <tyler.westlie@gmail.com>
2018-07-31 14:57:56 -0600

    more styling to the user dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :find_bookmark
   helper_method :list_tags
+  helper_method :tutorial_name
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -15,5 +16,9 @@ class ApplicationController < ActionController::Base
     tutorials.map do |tutorial|
       tutorial.tag_list
     end.flatten(1)
+  end
+
+  def tutorial_name(id)
+    Tutorial.find(id).title
   end
 end
