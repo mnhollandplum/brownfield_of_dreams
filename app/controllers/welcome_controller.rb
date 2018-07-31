9a7f6fcfb0b71b0e2a232a95b9029e5181080ef1
Tyler Westlie <tyler.westlie@gmail.com>
2018-07-31 15:50:03 -0600

    tags now persist between pages, added reset button to filters

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,13 +2,8 @@ class WelcomeController < ApplicationController
   def index
     if params[:tag]
       @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
-      @list = list_tags(@tutorials).uniq
-    elsif params[:search]
-      @tutorials = Tutorial.search(params[:search]).order("created_at DESC")
-      @list = list_tags(@tutorials).uniq
     else
       @tutorials = Tutorial.all.paginate(:page => params[:page], :per_page => 5)
-      @list = list_tags(@tutorials).uniq
     end
   end
 end
