b3dd8eee8b4c2667ab3b0bc3772a496f77878a19
Tyler Westlie <tyler.westlie@gmail.com>
2018-07-31 22:35:36 -0600

    refactored tutorial controller

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -7,8 +7,5 @@ class TutorialsController < ApplicationController
   def show
     tutorial = Tutorial.find(params[:id])
     @presenter = TutorialPresenter.new(tutorial, params[:video_id])
-    if @presenter.next_video_position >= @presenter.position_array.length
-      flash[:success] == "You did it"
-    end
   end
 end
