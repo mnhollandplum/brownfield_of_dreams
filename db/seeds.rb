8dc7cd07daa394708325e711e6fcc642cc7f011c
Katelyn <katelyn.rimkunas@gmail.com>
2018-12-04 14:58:16 -0700

    update user table and seed a test user

--- db/seeds.rb
+++ db/seeds.rb
@@ -116,3 +116,4 @@ m3_tutorial.videos.create!({
 })
 
 User.create!(email: 'admin@example.com', first_name: 'Bossy', last_name: 'McBosserton', password:  "password", role: :admin)
+User.create!(email: 'user', first_name: 'John', last_name: 'Doe', password:  "user", role: :default)
