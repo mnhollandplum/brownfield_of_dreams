e697fa234405634cc4abdbbee521508f487b0fab
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-12-06 15:55:07 -0700

    passing feature test for github user repos

--- app/facades/dashboard_facade.rb
+++ app/facades/dashboard_facade.rb
@@ -1,12 +1,14 @@
 require './app/facades/github_user_facade'
 
 class DashboardFacade
+  attr_reader :user
   def initialize(user)
     @user = user
   end
 
   def github_user
-    @github_user ||= GithubUserFacade.new(@user.github_token)
+    return nil if @user.github_token == nil
+    @github_user ||=  GithubUserFacade.new(@user.github_token)
   end
 
   def list_repos
--- spec/features/user/user_sees_repos_spec.rb
+++ spec/features/user/user_sees_repos_spec.rb
@@ -1,38 +1,39 @@
 require 'rails_helper'
 
 describe 'User' do
-  before(:each) do
-    stub_user_repo_api_requests
-    stub_user_followers_api_requests
-    stub_user_following_api_requests
-  end
-
-    VCR.use_cassette("github_token") do
-      it 'user can sign in' do
-        user = create(:user)
+  describe 'Github User' do
+    before(:each) do
+      stub_user_repo_api_requests
+      stub_user_followers_api_requests
+      stub_user_following_api_requests
+    end
+        it 'user can see github repos in' do
+        user =  User.create!(email: "user@mail.com", first_name: "test", last_name: "user", password: "password", github_token: ENV["github_user_token"])
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+        visit dashboard_path
+
+        expect(page).to have_content(user.first_name)
+        expect(page).to have_content(user.last_name)
+
+        expect(page).to have_content("Github")
+        expect(page).to have_css(".github-repos li", count: 5)
+      end
+  end
+  describe 'Non-Github User' do
+
+    it 'does not see the "Github" section if the user is missing a Github token' do
+      no_github = User.create!(email: "user@mail.com", first_name: "test", last_name: "user", password: "password", github_token: nil)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(no_github)
 
       visit dashboard_path
 
-      expect(page).to have_content(user.first_name)
-      expect(page).to have_content(user.last_name)
+  		expect(page).to have_content(no_github.first_name)
+  		expect(page).to have_content(no_github.last_name)
 
-      expect(page).to have_content("Github")
-      expect(page).to have_css(".user_repos", count: 5)
+  		expect(page).to_not have_content("Repositories")
+  		expect(page).to_not have_content("Github")
     end
-  end
-
-  it 'does not see the "Github" section if the user is missing a Github token' do
-    no_github = User.create!(email: "not a github user", first_name: "test", last_name: "user", password: "password", github_token: nil)
-
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(no_github)
-
-    visit dashboard_path
-    save_and_open_page
-
-		expect(page).to have_content(no_github.first_name)
-		expect(page).to have_content(no_github.last_name)
-
-		expect(page).to_not have_content("Github Repositories")
   end
 end
