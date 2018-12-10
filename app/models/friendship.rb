60069519fe097b55670fa0f6b713157a53f3eaed
Katelyn <katelyn.rimkunas@gmail.com>
2018-12-10 14:45:33 -0700

    user has friends

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,7 +3,9 @@ class Friendship < ApplicationRecord
 
   belongs_to :user
   belongs_to :friend, class_name: "User"
-  # is friend going to be an issue if using friend_id in the model ???
+
+  validates_uniqueness_of :friend, :scope => [:user]
+  # validates_uniqueness_of :user, :scope => [:friend]
 
 
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,28 +2,30 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
-    it {should validate_presence_of(:email)}
-    it {should validate_presence_of(:first_name)}
-    it {should validate_presence_of(:password)}
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :password }
   end
 
   describe "Relationships" do
     it { should have_many :user_videos }
     it { should have_many :videos }
+
     it { should have_one  :github }
+
+    it { should have_many :friendships }
+    it { should have_many(:friends).through(:friendships) }
   end
 
   describe 'roles' do
     it 'can be created as default user' do
       user = User.create(email: 'user@email.com', password: 'password', first_name:'Jim', role: 0)
-
       expect(user.role).to eq('default')
       expect(user.default?).to be_truthy
     end
 
     it 'can be created as an Admin user' do
       admin = User.create(email: 'admin@email.com', password: 'admin', first_name:'Bob', role: 1)
-
       expect(admin.role).to eq('admin')
       expect(admin.admin?).to be_truthy
     end
@@ -46,6 +48,44 @@ RSpec.describe User, type: :model do
     it "should have github_username" do
       expect(@user.github_username).to eq(@github.username)
     end
+
+  end
+
+  describe "Friendships" do
+
+    before(:each) do
+      @user    = User.create(email: 'user@email.com',    password: 'password', first_name:'Jim',  role: 0)
+      @friend1 = User.create(email: 'friend1@email.com', password: 'password', first_name:'friend1', role: 0)
+      @friend2 = User.create(email: 'friend2@email.com', password: 'password', first_name:'friend2', role: 0)
+    end
+
+
+    it "Should have friends" do
+      expect(@user.friendships.count).to eq(0)
+      @user.friendships.create(friend: @friend1)
+      expect(@user.friendships.count).to eq(1)
+      @user.friendships.create(friend: @friend2)
+      expect(@user.friendships.count).to eq(2)
+
+      expect(@user.friends.count).to eq(2)
+      expect(@user.friends.first).to eq(@friend1)
+      expect(@user.friends.last).to  eq(@friend2)
+    end
+
+    it 'Should not be able to add the same friend more than once' do
+      expect(@user.friendships.count).to eq(0)
+      @user.friendships.create(friend: @friend1)
+      expect(@user.friendships.count).to eq(1)
+      @user.friendships.create(friend: @friend1)
+      expect(@user.friendships.count).to eq(1)
+      @user.friendships.create(friend: @friend2)
+      expect(@user.friendships.count).to eq(2)
+
+      expect(@user.friends.count).to eq(2)
+      expect(@user.friends.first).to eq(@friend1)
+      expect(@user.friends.last).to  eq(@friend2)
+    end
+
 
   end
 
