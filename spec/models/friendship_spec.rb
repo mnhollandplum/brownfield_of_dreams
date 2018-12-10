a634d47b55d80f74ee44c42edc11b0f366a613f8
Katelyn <katelyn.rimkunas@gmail.com>
2018-12-10 14:49:27 -0700

    friendship has tests

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -7,4 +7,23 @@ RSpec.describe Friendship, type: :model do
     it { should belong_to :friend }
   end
 
+  describe "can return user and friend objects" do
+
+    before(:each) do
+      @user    = User.create(email: 'user@email.com',    password: 'password', first_name:'Jim',  role: 0)
+      @friend1 = User.create(email: 'friend1@email.com', password: 'password', first_name:'friend1', role: 0)
+      @friendship = Friendship.create(user: @user, friend: @friend1)
+    end
+
+    it 'Should return the user' do
+      expect(@friendship.user).to eq(@user)
+    end
+
+    it 'Should return the friend' do
+      expect(@friendship.friend).to eq(@friend1)
+    end
+
+
+  end
+
 end
