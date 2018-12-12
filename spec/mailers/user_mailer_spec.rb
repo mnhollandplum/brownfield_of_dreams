77855e39b1ba7efd13f6db0af8d1ac486f637b91
Katelyn <katelyn.rimkunas@gmail.com>
2018-12-11 22:16:59 -0700

    tests for mailer

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,17 +1,28 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
+
   describe "account_activation" do
-    let(:mail) { UserMailer.account_activation }
+
+    before(:each) do
+      @user = create(:active_user)
+      @user.activation_token = User.new_token
+      @mail = UserMailer.account_activation( @user )
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return( @user )
+    end
+
+    let(:mail) { UserMailer.account_activation(@user) }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Account activation")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(@mail.subject).to eq( "Account Activation" )
+      expect(@mail.to).to      eq( [@user.email] )
+      expect(@mail.from).to    eq( ["no-reply@bfod.io"] )
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(@mail.body.encoded).to match( @user.first_name )
+      expect(@mail.body.encoded).to match( @user.activation_token )
+      expect(@mail.body.encoded).to match( CGI.escape( @user.email ) )
     end
   end
 
