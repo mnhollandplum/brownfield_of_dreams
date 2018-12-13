8b760ed05f9fa87fead0608a49044524d06c4870
Katelyn <katelyn.rimkunas@gmail.com>
2018-12-12 21:19:21 -0700

    show page restricted for class content

--- spec/features/user/visitor_does_not_see_classroom_content_spec.rb
+++ spec/features/user/visitor_does_not_see_classroom_content_spec.rb
@@ -9,23 +9,36 @@ describe 'Classroom Content (Tutorials)' do
 
 
   describe 'Visitor' do
-    it 'Does not display classroom content' do
+    it 'Root does not display classroom content' do
       visit '/'
 
       expect(page).to     have_content(@other.title)
       expect(page).to_not have_content(@classroom.title)
     end
+
+    it 'Show does not display classroom content' do
+      expect{ visit tutorial_path(@classroom) }.to raise_error(ActionController::RoutingError)
+      expect{ visit tutorial_path(@other) }.to_not raise_error(ActionController::RoutingError)
+    end
   end
 
   describe 'User' do
-    it "Displays classroom content " do
+
+    before(:each) do
       user = create(:user)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    end
 
+    it "Displays classroom content " do
       visit '/'
 
       expect(page).to have_content(@other.title)
       expect(page).to have_content(@classroom.title)
+    end
+
+    it 'Show can display classroom content' do
+      expect{ visit tutorial_path(@other)     }.to_not raise_error(ActionController::RoutingError)
+      expect{ visit tutorial_path(@classroom) }.to_not raise_error(ActionController::RoutingError)
     end
   end
 
