31659b95dc64c1206fee5104227e869647e34d29
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-12-13 18:03:11 -0700

    updates sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,8 +103,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['sendgrid_username'],
-    :password       => ENV['sendgrid_password'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
